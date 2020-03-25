0c24e2ddf8c9a3ca91d6025dd3c7aae4c6a22560
AaronKajenderan <aaronriboy@gmail.com>
2020-03-25 20:08:40 +0800

    pet tracking UI done

--- RSPi2-Home-Domotic-System-master/PetPi/app/src/main/java/com/example/petpi/Dashboard.java
+++ RSPi2-Home-Domotic-System-master/PetPi/app/src/main/java/com/example/petpi/Dashboard.java
@@ -1,14 +1,32 @@
 package com.example.petpi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class Dashboard extends AppCompatActivity {
+
+    private Object CardView;
+
+    public void StartTrack (View view){
+        Intent intent= new Intent(Dashboard.this,MainActivity.class);
+        startActivity(intent);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        CardView =(CardView) findViewById(R.id.track);
+
+
+
+
     }
 }
---
+++ RSPi2-Home-Domotic-System-master/PetPi/app/src/main/java/com/example/petpi/monitorPet.java
@@ -0,0 +1,14 @@
+package com.example.petpi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class monitorPet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
